662fa6c9e86f865e5df9e058d4614f3e3f47f6b2
Pavel Leonov <paskas@gmail.com>
2018-03-07 16:53:08 +0200

    update

--- Task.py
+++ Task.py
@@ -16,4 +16,3 @@ class Task():
     myFilter = ''
     def __init__(self):
         return
-
--- bot.py
+++ bot.py
@@ -2,10 +2,10 @@ import telebot
 import bs4
 from Task import Task
 import parser
-
+import markups as m
 
 #main variables
-TOKEN = "565432894:AAGVPLxxS1v-KgBjYZnKmZ0XFfRL85-ZS1Y"
+TOKEN = ''
 bot = telebot.TeleBot(TOKEN)
 task = Task()
 
@@ -14,7 +14,7 @@ task = Task()
 def start_handler(message):
     if not task.isRunning:
         chat_id = message.chat.id
-        msg = bot.send_message(chat_id, 'Откуда парсить?')
+        msg = bot.send_message(chat_id, 'Откуда парсить?', reply_markup=m.source_markup)
         bot.register_next_step_handler(msg, askSource)
         task.isRunning = True
 
@@ -23,11 +23,11 @@ def askSource(message):
     text = message.text.lower()
     if text in task.names[0]:
         task.mySource = 'top'
-        msg = bot.send_message(chat_id, 'За какой временной промежуток?')
+        msg = bot.send_message(chat_id, 'За какой временной промежуток?', reply_markup=m.age_markup)
         bot.register_next_step_handler(msg, askAge)
     elif text in task.names[1]:
         task.mySource = 'all'
-        msg = bot.send_message(chat_id, 'Какой минимальный порог рейтинга?')
+        msg = bot.send_message(chat_id, 'Какой минимальный порог рейтинга?', reply_markup=m.rating_markup)
         bot.register_next_step_handler(msg, askRating)
     else:
         msg = bot.send_message(chat_id, 'Такого раздела нет. Введите раздел корректно.')
@@ -43,7 +43,7 @@ def askAge(message):
         bot.register_next_step_handler(msg, askAge)
         return
     task.myFilter = task.filters_code_names[0][filters.index(text)]
-    msg = bot.send_message(chat_id, 'Сколько страниц парсить?')
+    msg = bot.send_message(chat_id, 'Сколько страниц парсить?', reply_markup=m.amount_markup)
     bot.register_next_step_handler(msg, askAmount)
 
 def askRating(message):
@@ -55,7 +55,7 @@ def askRating(message):
         bot.register_next_step_handler(msg, askRating)
         return
     task.myFilter = task.filters_code_names[1][filters.index(text)]
-    msg = bot.send_message(chat_id, 'Сколько страниц парсить?')
+    msg = bot.send_message(chat_id, 'Сколько страниц парсить?', reply_markup=m.amount_markup)
     bot.register_next_step_handler(msg, askAmount)
 
 def askAmount(message):
@@ -65,17 +65,19 @@ def askAmount(message):
         msg = bot.send_message(chat_id, 'Количество страниц должно быть числом. Введите корректно.')
         bot.register_next_step_handler(msg, askAmount)
         return
-    if int(text) < 1 or int(text) > 11:
-        msg = bot.send_message(chat_id, 'Количество страниц должно быть >0 и <11. Введите корректно.')
+    if int(text) < 1 or int(text) > 5:
+        msg = bot.send_message(chat_id, 'Количество страниц должно быть >0 и <6. Введите корректно.')
         bot.register_next_step_handler(msg, askAmount)
         return
     task.isRunning = False
+    print(task.mySource + " | " + task.myFilter + ' | ' + text) #
     output = ''
     if task.mySource == 'top':
         output = parser.getTitlesFromTop(int(text), task.myFilter)
     else:
         output = parser.getTitlesFromAll(int(text), task.myFilter)
-    msg = bot.send_message(chat_id, output)
+    msg = bot.send_message(chat_id, output, reply_markup=m.start_markup)
 
 bot.polling(none_stop=True)
+
 
